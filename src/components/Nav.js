2f52e4f0c642d6942456a812e2c1a2fcbb55701a
pjuric <72385188+pjuric@users.noreply.github.com>
2021-06-04 23:19:52 +0200

    temporary nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,15 @@ function Nav() {
 
 
     return (
-        <header className="absolute top-0 flex justify-between sm:flex-row p-5 md:justify-between items-center z-50 w-full">
+        <header className="absolute top-0 flex justify-between p-5 items-center z-50 w-full">
             <a href="/"><img className="w-10 md:w-14 z-50" src="./images/logo.svg" alt="logo"/></a>
-            <div className="hidden md:flex flex-grow justify-evenly max-w-2xl text-white-secondary">
-                <a className="hover:text-white-primary z-50 font-medium" href="/">Home</a>
-                <a className="hover:text-white-primary z-50 font-medium" href="/search">Search</a>
-                <a className="hover:text-white-primary z-50 font-medium" href="/">Credits</a>
-                <a className="hover:text-white-primary z-50 font-medium" href="/">About</a>
+            <div className="flex flex-grow justify-evenly max-w-2xl text-white-secondary">
+                <a className="hover:text-white-primary z-50 text-xs md:text-base" href="/">Home</a>
+                <a className="hover:text-white-primary z-50 text-xs md:text-base" href="/search">Search</a>
+                <a className="hover:text-white-primary z-50 text-xs md:text-base" href="/">Credits</a>
+                <a className="hover:text-white-primary z-50 text-xs md:text-base" href="/">About</a>
             </div>
-            <div className="w-10 overflow-hidden h-10 md:w-14 md:h-14 md:hidden z-10">
+            <div className="w-10 hidden overflow-hidden h-10 md:w-14 md:h-14 md:hidden z-10">
                 <button className=""><MenuIcon fontSize="large"/></button>
             </div>
         </header>
